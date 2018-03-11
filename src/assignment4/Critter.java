d7806934c2827b7c6b5f0a0ceb4972981558b48e
rebeccaphung <rebecca.phung@utexas.edu>
2018-03-11 16:28:15 -0500

    programmed walk and run

--- src/assignment4/Critter.java
+++ src/assignment4/Critter.java
@@ -30,39 +30,110 @@ public abstract class Critter {
 	static {
 		myPackage = Critter.class.getPackage().toString().split(" ")[1];
 	}
-	
+
 	private static java.util.Random rand = new java.util.Random();
 	public static int getRandomInt(int max) {
 		return rand.nextInt(max);
 	}
-	
+
 	public static void setSeed(long new_seed) {
 		rand = new java.util.Random(new_seed);
 	}
-	
-	
+
+
 	/* a one-character long string that visually depicts your critter in the ASCII interface */
 	public String toString() { return ""; }
-	
+
 	private int energy = 0;
 	protected int getEnergy() { return energy; }
-	
+
 	private int x_coord;
 	private int y_coord;
-	
+
+	private final void changeLocation(int xChange, yChange){
+		x_coord += xChange;
+		if(x_coord > Params.world_width - 1){
+			x_coord = x_coord % Params.world_width;
+		}
+		else if(x_coord < 0){
+			x_coord += Params.world_width;
+		}
+
+		y_coord += yChange;
+		if(y_coord > Params.world_height - 1){
+			y_coord = y_coord % Params.world_height;
+		}
+		else if(y_coord < 0){
+			y_coord += Params.world_height;
+		}
+	}
+
 	protected final void walk(int direction) {
+		energy -= Param.walk_energy_cost;
+		switch (direction){
+			case 0:
+				changeLocation(1, 0);
+				break;
+			case 1:
+				changeLocation(1, 1);
+				break;
+			case 2:
+				changeLocation(0, 1);
+				break;
+			case 3:
+				changeLocation(-1, 1);
+				break;
+			case 4:
+				changeLocation(-1, 0);
+				break;
+			case 5:
+				changeLocation(-1, -1);
+				break;
+			case 6:
+				changeLocation(0, -1);
+				break;
+			case 7:
+				changeLocation(1, -1);
+				break;
+		}
 	}
-	
+
 	protected final void run(int direction) {
-		
+		energy -= Param.run_energy_cost;
+		switch (direction){
+			case 0:
+				changeLocation(2, 0);
+				break;
+			case 1:
+				changeLocation(2, 2);
+				break;
+			case 2:
+				changeLocation(0, 2);
+				break;
+			case 3:
+				changeLocation(-2, 2);
+				break;
+			case 4:
+				changeLocation(-2, 0);
+				break;
+			case 5:
+				changeLocation(-2, -2);
+				break;
+			case 6:
+				changeLocation(0, -2);
+				break;
+			case 7:
+				changeLocation(2, -2);
+				break;
+		}
 	}
-	
+
 	protected final void reproduce(Critter offspring, int direction) {
 	}
 
 	public abstract void doTimeStep();
 	public abstract boolean fight(String oponent);
-	
+
 	/**
 	 * create and initialize a Critter subclass.
 	 * critter_class_name must be the unqualified name of a concrete subclass of Critter, if not,
@@ -75,7 +146,7 @@ public abstract class Critter {
 	 */
 	public static void makeCritter(String critter_class_name) throws InvalidCritterException {
 	}
-	
+
 	/**
 	 * Gets a list of critters of a specific type.
 	 * @param critter_class_name What kind of Critter is to be listed.  Unqualified class name.
@@ -84,10 +155,10 @@ public abstract class Critter {
 	 */
 	public static List<Critter> getInstances(String critter_class_name) throws InvalidCritterException {
 		List<Critter> result = new java.util.ArrayList<Critter>();
-	
+
 		return result;
 	}
-	
+
 	/**
 	 * Prints out how many Critters of each type there are on the board.
 	 * @param critters List of Critters.
@@ -109,13 +180,13 @@ public abstract class Critter {
 			System.out.print(prefix + s + ":" + critter_count.get(s));
 			prefix = ", ";
 		}
-		System.out.println();		
+		System.out.println();
 	}
-	
-	/* the TestCritter class allows some critters to "cheat". If you want to 
+
+	/* the TestCritter class allows some critters to "cheat". If you want to
 	 * create tests of your Critter model, you can create subclasses of this class
-	 * and then use the setter functions contained here. 
-	 * 
+	 * and then use the setter functions contained here.
+	 *
 	 * NOTE: you must make sure that the setter functions work with your implementation
 	 * of Critter. That means, if you're recording the positions of your critters
 	 * using some sort of external grid or some other data structure in addition
@@ -126,23 +197,23 @@ public abstract class Critter {
 		protected void setEnergy(int new_energy_value) {
 			super.energy = new_energy_value;
 		}
-		
+
 		protected void setX_coord(int new_x_coord) {
 			super.x_coord = new_x_coord;
 		}
-		
+
 		protected void setY_coord(int new_y_coord) {
 			super.y_coord = new_y_coord;
 		}
-		
+
 		protected int getX_coord() {
 			return super.x_coord;
 		}
-		
+
 		protected int getY_coord() {
 			return super.y_coord;
 		}
-		
+
 
 		/*
 		 * This method getPopulation has to be modified by you if you are not using the population
@@ -152,11 +223,11 @@ public abstract class Critter {
 		protected static List<Critter> getPopulation() {
 			return population;
 		}
-		
+
 		/*
 		 * This method getBabies has to be modified by you if you are not using the babies
 		 * ArrayList that has been provided in the starter code.  In any case, it has to be
-		 * implemented for grading tests to work.  Babies should be added to the general population 
+		 * implemented for grading tests to work.  Babies should be added to the general population
 		 * at either the beginning OR the end of every timestep.
 		 */
 		protected static List<Critter> getBabies() {
@@ -170,11 +241,11 @@ public abstract class Critter {
 	public static void clearWorld() {
 		// Complete this method.
 	}
-	
+
 	public static void worldTimeStep() {
 		// Complete this method.
 	}
-	
+
 	public static void displayWorld() {
 		// Complete this method.
 	}
