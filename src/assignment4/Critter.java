10229eafc42ffd5f04604a86f101b0bc27c2213d
Richard Li <esealp@gmail.com>
2018-04-01 16:31:03 -0500

    Part 2 starter

--- src/assignment4/Critter.java
+++ src/assignment4/Critter.java
@@ -14,6 +14,7 @@ package assignment4;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import javafx.*;
 
 
 /* see the PDF for descriptions of the methods and fields in this class
@@ -23,6 +24,34 @@ import java.util.List;
 
 
 public abstract class Critter {
+	public enum CritterShape {
+		CIRCLE,
+		SQUARE,
+		TRIANGLE,
+		DIAMOND,
+		STAR
+	}
+	
+	/* the default color is white, which I hope makes critters invisible by default
+	 * If you change the background color of your View component, then update the default
+	 * color to be the same as you background 
+	 * 
+	 * critters must override at least one of the following three methods, it is not 
+	 * proper for critters to remain invisible in the view
+	 * 
+	 * If a critter only overrides the outline color, then it will look like a non-filled 
+	 * shape, at least, that's the intent. You can edit these default methods however you 
+	 * need to, but please preserve that intent as you implement them. 
+	 */
+	public javafx.scene.paint.Color viewColor() { 
+		return javafx.scene.paint.Color.WHITE; 
+	}
+	
+	public javafx.scene.paint.Color viewOutlineColor() { return viewColor(); }
+	public javafx.scene.paint.Color viewFillColor() { return viewColor(); }
+	
+	public abstract CritterShape viewShape(); 
+	
 	private static String myPackage;
 	private	static List<Critter> population = new java.util.ArrayList<Critter>();
 	private static List<Critter> babies = new java.util.ArrayList<Critter>();
@@ -32,6 +61,8 @@ public abstract class Critter {
 		myPackage = Critter.class.getPackage().toString().split(" ")[1];
 	}
 
+	protected final String look(int direction, boolean steps) {return "";}
+	
 	private static java.util.Random rand = new java.util.Random();
 	/*
 	 * Random int Generator
--- src/assignment4/Params.java
+++ src/assignment4/Params.java
@@ -20,5 +20,6 @@ public class Params {
 	public  static int refresh_algae_count = 10;
 	public static  int photosynthesis_energy_amount = 1;
 	public static  int start_energy = 100;
+	public static int look_energy_cost = 1;
 
 }
